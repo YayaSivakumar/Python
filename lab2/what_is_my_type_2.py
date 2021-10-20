68c466fabae03813b4db1b14b32899f6c2a8242d
YayaSivakumar <yaya.s@hotmail.co.uk>
2021-10-20 11:37:29 +0100

    k

--- lab2/what_is_my_type_2.py
+++ lab2/what_is_my_type_2.py
@@ -2,9 +2,12 @@
 # Yachitra Sivakumar, 121373093
 
 def type_of_variable(question):
-    # determines type of the variable
+    '''
+    determines type of variable
+    returns the variable and the variables type
+    returns id of the variable
+    '''
     my_type = type(question)
-    # returns the type to user
     return print("The variable is", question, "it is of type", my_type, "and has and id of", id(my_type))
 
 type_of_variable(9)
